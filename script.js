8495982b05d35d804e867d2bc5ff5c5ee5cd5490
1Heyzeus <148654475+1Heyzeus@users.noreply.github.com>
2023-10-28 20:11:43 +0100

    Format changes

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
 //Randomly generating computer's choice
 function getComputerSelection() { 
 const choiceArray = ['rock', 'paper', 'scissors'];
 return choiceArray[(Math.floor(Math.random()*choiceArray.length))];
 }
+
 let compWins = 0;
 let humanWins = 0;
+
 //function to display score
 function score() {
     console.log(humanWins);
